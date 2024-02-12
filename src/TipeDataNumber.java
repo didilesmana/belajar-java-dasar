ac306c0b426e66f976e61da5dc2fbc6fe21934eb
didilesmana <22350558+didilesmana@users.noreply.github.com>
2024-02-12 10:24:13 +0700

    new file

---
+++ src/TipeDataNumber.java
@@ -0,0 +1,30 @@
+public class TipeDataNumber {
+    public static void main(String[] args) {
+        byte iniByte = 100;
+        short iniShort = 1000;
+        int iniInt = 1000000;
+        long iniLong = 1000000;
+        long iniLong2 = 1000000L;
+        float iniFloat = 10.12F;
+        double iniDouble = 12.2424;
+        int decimalInt = 25;
+        int hexInt = 0xA132B;
+        int binInt = 0b1010101;
+        long balance = 1_000_000_000_000L;
+        int sum = 60_000_000;
+
+        System.out.println(iniByte);
+        System.out.println(iniShort);
+        System.out.println(iniInt);
+        System.out.println(iniLong);
+        System.out.println(iniLong2);
+        System.out.println(iniFloat);
+        System.out.println(iniDouble);
+        System.out.println(decimalInt);
+        System.out.println(hexInt);
+        System.out.println(binInt);
+        System.out.println(balance);
+        System.out.println(sum);
+
+    }
+}
